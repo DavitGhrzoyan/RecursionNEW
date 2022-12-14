f1326e3537dcf799106ce85d9dfbaab3b2a712e6
Davit <ghrzoyandavit@gmail.com>
2022-12-14 21:42:06 +0400

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        //Recursia 1
+        static void Rec(int i)
+        {
+            Console.WriteLine(i);
+            if (i >= 10)
+                 return;
+            i++;
+            Rec(i);
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recursion is in process");
+            Rec(0);
+        }
+
+
+    }
+}
